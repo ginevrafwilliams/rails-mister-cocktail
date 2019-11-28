dde073717e0978d1e315e4e4e23b0a0771f6aa7d
Ginevra Fontes Williams <ginevra.f.williams@gmail.com>
2019-11-28 17:29:46 +0100

    done

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,21 @@
+class DosesController < ApplicationController
+  def new
+  @cocktail = params[:id]
+  @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail.id)
+    else
+      render :new
+    end
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
